c959d7840ecf5cb2a7b73552dfbdc0cf55aebdfd
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-01-18 02:43:28 -0700

    Added Week-2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Kshitij Tagra</h1>
+      <Link href="https://github.com/kshitij-tagra/cprg306-assignments">
+        https://github.com
+      </Link>
+    </main>
+  );
+}
