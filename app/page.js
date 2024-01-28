65ec1ee9486d9b4627f1cbd527242f056ed1439e
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-01-27 21:55:21 -0700

    Complete Week-3 Assignment

--- app/page.js
+++ app/page.js
@@ -3,8 +3,16 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="http://localhost:3000/week-2">Week 2 Assignment</Link>
+      <h1 className="text-4xl font-bold pt-24 pl-56 pb-5">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <Link href="week-2" className="text-xl pl-56">
+        Week 2 Assignment
+      </Link>
+      <br />
+      <Link href="week-3" className="text-xl pl-56">
+        Week 3 Assignment
+      </Link>
     </main>
   );
 }
